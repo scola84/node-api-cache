328e80937fc121855f4f3f1226dae2ffc77169e1
M.C. van den Boogaart <scola@kvdb.name>
2017-02-19 23:39:41 +0100

    Add convenience functions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import CacheClient from './src/client';
 
 export { default as Cache } from './src/cache';
+export { default as cacheList } from './src/helper/list';
+export { default as cacheObject } from './src/helper/object';
 export { default as getList } from './src/helper/list/get';
 export { default as setList } from './src/helper/list/set';
 export { default as getObject } from './src/helper/object/get';
