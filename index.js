a8d87f24a6b4899b52ac88f6dd95e0604a346961
M.C. van den Boogaart <scola@kvdb.name>
2017-03-03 21:25:28 +0100

    Refactor exports

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
+import Cache from './src/cache';
 import CacheClient from './src/client';
+import cacheList from './src/helper/list';
+import cacheObject from './src/helper/object';
+import getList from './src/helper/list/get';
+import setList from './src/helper/list/set';
+import getObject from './src/helper/object/get';
+import setObject from './src/helper/object/set';
+import getTotal from './src/helper/total/get';
+import setTotal from './src/helper/total/set';
 
-export { default as Cache } from './src/cache';
-export { default as cacheList } from './src/helper/list';
-export { default as cacheObject } from './src/helper/object';
-export { default as getList } from './src/helper/list/get';
-export { default as setList } from './src/helper/list/set';
-export { default as getObject } from './src/helper/object/get';
-export { default as setObject } from './src/helper/object/set';
-export { default as getTotal } from './src/helper/total/get';
-export { default as setTotal } from './src/helper/total/set';
-
-export function cacheClient() {
+function cacheClient() {
   return new CacheClient();
 }
+
+export {
+  Cache,
+  CacheClient,
+  cacheClient,
+  cacheList,
+  cacheObject,
+  getList,
+  setList,
+  getObject,
+  setObject,
+  getTotal,
+  setTotal
+};
